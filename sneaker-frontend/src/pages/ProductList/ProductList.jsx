e423f12300de433f78dd3f4a8f23035a5abfc054
Anh-Tu2912 <22130307@st.hcmuaf.edu.vn>
2026-05-19 13:04:25 +0700

    feat(ProductList): Thêm chức năng sắp xếp sản phẩm trang danh sách sản phẩm (Closes #40)

--- sneaker-frontend/src/pages/ProductList/ProductList.jsx
+++ sneaker-frontend/src/pages/ProductList/ProductList.jsx
@@ -8,15 +8,38 @@ import './ProductList.css'
 function ProductList() {
     const [products, setProducts] = useState([]);
 
+    // Lưu tiêu chí sắp xếp sản phẩm hiện tại của người dùng
+    const [sortOption, setSortOption] = useState("Featured");
+
     // Lấy dữ liệu từ cơ sở dữ liệu
     useEffect(() => {
-        fetch("http://localhost:8080/api/products")
+        let url = "http://localhost:8080/api/products"
+
+        // Kiểm tra lựa chọn của người dùng hiện tại để tạo ra URL phù hợp
+        // Điều kiện lọc sản phẩm hiện tại
+        switch (sortOption) {
+            case "Price: Low to High":
+                url += "?sortBy=price&sortDir=asc";
+                break;
+            case "Price: High to Low":
+                url += "?sortBy=price&sortDir=desc";
+                break;
+            case "Customer Review":
+                // Hiện tại chưa cần xử lí có thể xử lí tạm
+                break;
+            case "Featured":
+            default:
+                url += "?sortBy=id&sortDir=asc"; // Sắp xếp mặc định
+                break;
+        }
+
+        fetch(url)
             .then(res => res.json())
             .then(data => {
                 setProducts(data);
             })
             .catch(err => console.error(err));
-    }, []);
+    }, [sortOption]);
 
     return (
         <div className="product-list-container">
@@ -33,11 +56,11 @@ function ProductList() {
                 {/* Bộ lọc sắp xếp theo yêu cầu */}
                 <div className="product-list-arrange">
                     <label>Sort by:</label>
-                    <select>
-                        <option>Featured</option>
-                        <option>Price: Low to High</option>
-                        <option>Price: High to Low</option>
-                        <option>Customer Review</option>
+                    <select value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+                        <option value="Featured">Featured</option>
+                        <option value="Price: Low to High">Price: Low to High</option>
+                        <option value="Price: High to Low">Price: High to Low</option>
+                        <option value="Customer Review">Customer Review</option>
                     </select>
                 </div>
             </div>
